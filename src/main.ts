31aef0ee1b8bda22b9e7682e7eb68c9917ecfd3d
Hasan Iqbal <swapniliqbal@gmail.com>
2024-01-20 21:53:23 -0500

    feat: implement health check endpoint

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).send({ message: 'OK' });
+});
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
