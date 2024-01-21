581b9eb45ff5253177ad7515a488b026d1ae1bb9
Hasan Iqbal <swapniliqbal@gmail.com>
2024-01-20 21:52:44 -0500

    feat: init cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT || 4000;
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
-});
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
