5b80cb76f50446ccc28ec1b19b2ef7a56265441c
Hasan Iqbal <swapniliqbal@gmail.com>
2024-01-25 16:43:47 -0500

    feat: init winston logger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,43 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import webhooksRoutes from './routes/webhooksRoutes';
+import winston from "winston";
+import expressWinston from "express-winston";
 
 const port = process.env.PORT || 4000;
-
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    meta: false,
+    msg: "HTTP  ",
+    expressFormat: true,
+    colorize: false,
+    ignoreRoute: function (req, res) {
+      return false;
+    },
+  })
+);
+
+app.get("/status", (req: Request, res: Response) => {
+  res.status(200).json({ status: "OK" });
+});
 
 // Routes
 app.use("/api/webhooks", webhooksRoutes);
+
+// 404 
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' });
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
