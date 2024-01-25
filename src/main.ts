79b652f1d8860676c3c7db742ee1ae31a596b1ca
Hasan Iqbal <swapniliqbal@gmail.com>
2024-01-25 01:13:47 -0500

    fix: include limit for express.json and also bring in express.urlencoded for non-json POST data

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ const port = process.env.PORT || 4000;
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Hello API' });
